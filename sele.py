1252e32db3ce4c21cf2367af8c9c3bde82117c41
Akshat Zala <akshatzala@gmail.com>
2020-04-10 19:49:15 +0530

    Scraping Daily Asian Market data from cnn business

--- sele.py
+++ sele.py
@@ -1,4 +1,3 @@
-# from goto import goto, comefrom, label
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 import requests
@@ -7,21 +6,20 @@ driver = webdriver.Chrome("/home/akshatz/Documents/Python/chromedriver")
 driver.get("https://money.cnn.com/data/world_markets/asia/")
 # driver.maximize_window()
 index = driver.implicitly_wait(30000)
-table_row = driver.find_elements(By.XPATH, '/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody')
-tr=table_row[0].text
-# for index in tr:
-#     index.find_element(By.Xpath, 'tr[2]/td[2]').text
-driver.quit()
-#         title = driver.find_element(By.CLASS_NAME, "wsod_fleft")
-#         print(title.text)
-#         left = driver.find_element(By.ID, "wsod_quoteLeft").text
-#         print(left)
-#         right = driver.find_element(By.ID, "wsod_quoteRight").text
-#         print(right)
-#         n+1
-#         driver.back()
-#         i = i+1
-
-#         driver.quit()
-#     except:
-#         print("Error")
+row_size = len(driver.find_elements_by_xpath('/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr'))-1
+n = row_size
+print(n)
+counter = 2
+while counter < 8:
+    final_path = f'/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[{counter}]/td[2]/a'.format(counter)
+    print(driver.find_element_by_xpath(final_path).click())
+    driver.implicitly_wait(30000)
+    title = driver.find_element(By.TAG_NAME, "h1")
+    print(title.text)
+    left = driver.find_element(By.ID, "wsod_quoteLeft")
+    print(left.text)
+    right = driver.find_element(By.ID, "wsod_quoteRight")
+    print(right.text)
+    driver.back()
+    counter = counter + 1
+driver.quit()   
