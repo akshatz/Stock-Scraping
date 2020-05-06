03aae8544215d8f4e775533be7a579b915fbe11c
Akshat Zala HP <akshatzala@gmail.com>
2020-05-06 18:03:28 +0530

    Wed, May  6, 2020  6:03:28 PM

--- cnnMarketsInIST.py
+++ cnnMarketsInIST.py
@@ -118,7 +118,7 @@ def stockScraping():
                 driver.back()    
                 driver.quit()
                 print(style.GREEN + "Successfully run the program for European stock market")
-            elif  "17:32:00" < now_time and now_time < "17:35:00":
+            elif  "18:00:00" < now_time and now_time < "18:05:00":
                 driver.get('https://money.cnn.com/data/world_markets/americas/')
                 counter = 2
                 while counter <  5:
