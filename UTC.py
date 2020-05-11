e1f5278acd5b5c48781f8e93bebd80901f803d44
Akshat Zala <akshatzala@gmail.com>
2020-05-11 09:55:29 +0530

    Added few minutes

--- UTC.py
+++ UTC.py
@@ -22,7 +22,7 @@ def stockScraping():
             header = "Date, Time, Index, Previous close, Day Open, Day High, Day Low, LTP/Closing Prices\n"
             if  Path(file).stat().st_size == 0: 
                 f.write(header)          
-            if "02:30:00"< now_time  and now_time < "02:31:00":
+            if "02:30:00"< now_time  and now_time < "02:32:00":
                 driver.get("https://money.cnn.com/data/world_markets/asia/")
                 counter = 2
                 while counter < 4:
@@ -62,8 +62,7 @@ def stockScraping():
                     driver.back()
                     counter = counter + 1     
                 driver.quit()
-                # print(style.GREEN + "Successfully run the program for Asian stock market")
-            elif "04:30:00" < now_time and now_time < "04:31:00":
+            elif "04:30:00" < now_time and now_time < "04:32:00":
                 counter = 2
                 driver.get("https://money.cnn.com/data/world_markets/europe/")
                 while counter < 8:
@@ -119,8 +118,7 @@ def stockScraping():
                 f.write(price[0:5]+"."+price[5:7]+"," +price[7:12]+"."+price[12:14]+","+price[14:19]+"."+price[19:21]+","+price[21:26]+"."+price[26:]+","+ltp+"\n")
                 driver.back()    
                 driver.quit()
-                # print(style.GREEN + "Successfully run the program for European stock market")
-            elif "11:30:00" < now_time and now_time < "11:31:00":
+            elif "11:30:00" < now_time and now_time < "11:32:00":
                 driver.get('https://money.cnn.com/data/world_markets/americas/')
                 counter = 2
                 while counter <  5:
