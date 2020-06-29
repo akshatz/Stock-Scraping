501c61679236e6edbf0647fd20a702c47cb46662
Akshat Zala <akshatzala@gmail.com>
2020-06-29 10:26:04 +0530

    Added while loop in europe

--- UTC.py
+++ UTC.py
@@ -70,7 +70,7 @@ def stockScraping():
             elif "04:30:00" < now_time and now_time < "04:32:00":
                 counter = 2
                 driver.get("https://money.cnn.com/data/world_markets/europe/")
-                while counter < 8:
+                while counter < 5:
                     final_path = f'/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[{counter}]/td[2]/a'.format(counter)
                     f.write(now_date+","+driver.find_element_by_xpath(final_path).text+",")
                     driver.find_element_by_xpath(final_path).click()
@@ -87,7 +87,7 @@ def stockScraping():
                     ltp = ltp.replace(",", '')            
                     f.write(price[0:4]+"."+price[4:6]+"," +price[6:10]+"."+price[10:12]+","+price[12:16]+"."+price[16:18]+","+price[18:22]+"."+price[22:]+","+ltp+"\n")
                     driver.back()
-                    counter = counter +2
+                    counter = counter + 2
                 counter = 3
                 final_path = f'/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[{counter}]/td[2]/a'.format(counter)
                 f.write(now_date+","+driver.find_element_by_xpath(final_path).text+",")
@@ -106,22 +106,25 @@ def stockScraping():
                 f.write(price[0:3]+"."+price[3:5]+"," +price[5:8]+"."+price[8:10]+","+price[10:13]+"."+price[13:15]+","+price[15:18]+"."+price[18:20]+","+ltp+"\n")
                 driver.back()
                 counter = 5
-                final_path = f'/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[{counter}]/td[2]/a'.format(counter)
-                f.write(now_date+","+driver.find_element_by_xpath(final_path).text+",")
-                driver.find_element_by_xpath(final_path).click()
-                driver.implicitly_wait(30000)
-                title = driver.find_element(By.TAG_NAME, "h1")
-                left = driver.find_element(By.ID, "wsod_quoteLeft")
-                price = []
-                string = left.text
-                for k in string:
-                    if k.isdigit() == True:
-                        price.append(k)
-                price = ''.join(price)
-                ltp = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/table/tbody/tr/td[1]/span").text
-                ltp = ltp.replace(",", '')    
-                f.write(price[0:5]+"."+price[5:7]+"," +price[7:12]+"."+price[12:14]+","+price[14:19]+"."+price[19:21]+","+price[21:26]+"."+price[26:]+","+ltp+"\n")
-                driver.back()    
+                while counter < 7:
+                    final_path = f'/html/body/div[3]/div[1]/div[1]/div[2]/table/tbody/tr[{counter}]/td[2]/a'.format(counter)
+                    f.write(now_date+","+driver.find_element_by_xpath(final_path).text+",")
+                    driver.find_element_by_xpath(final_path).click()
+                    driver.implicitly_wait(30000)
+                    title = driver.find_element(By.TAG_NAME, "h1")
+                    left = driver.find_element(By.ID, "wsod_quoteLeft")
+                    price = []
+                    string = left.text
+                    for k in string:
+                        if k.isdigit() == True:
+                            price.append(k)
+                    price = ''.join(price)
+                    ltp = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[2]/table/tbody/tr/td[1]/span").text
+                    ltp = ltp.replace(",", '')    
+                    f.write(price[0:5]+"."+price[5:7]+"," +price[7:12]+"."+price[12:14]+","+price[14:19]+"."+price[19:21]+","+price[21:26]+"."+price[26:]+","+ltp+"\n")
+                    
+                    counter = counter + 1
+                    driver.back()    
                 driver.quit()
             elif "11:30:00" < now_time and now_time < "11:32:00":
                 driver.get('https://money.cnn.com/data/world_markets/americas/')
